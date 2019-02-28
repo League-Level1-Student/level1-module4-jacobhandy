8f2b06979d129de9dd5d776ca6b8d83fc8a19624
student <student@SEMS-259-31.SEMS259.LAB>
2019-02-27 16:27:37 -0800

    your mom

--- src/TypingTutor.java
+++ src/TypingTutor.java
@@ -1,20 +1,28 @@
 import java.awt.Color;
 import java.awt.event.KeyEvent;
 import java.awt.event.KeyListener;
+import java.util.Date;
 import java.util.Random;
+import java.util.concurrent.TimeUnit;
 
 import javax.swing.JFrame;
 import javax.swing.JLabel;
+import javax.swing.JOptionPane;
 import javax.swing.JPanel;
 
 public class TypingTutor implements KeyListener {
 JFrame frame;
 JLabel letter;
 String a;
-
+int charactersTyped = 0;
+int seconds = 0;
+int correctChar = 0;
+int time = 0;
 char currentLetter;
 public void createUI() {
-	frame = new JFrame();
+	  
+	
+     frame = new JFrame();
 	frame.addKeyListener(this);
 	letter = new JLabel();
 	frame.add(letter);
@@ -28,6 +36,7 @@ public void createUI() {
 	a = Character.toString(currentLetter);
 	letter.setText(a);
 	frame.pack();
+	 
 }
 char generateRandomLetter() {
     Random r = new Random();
@@ -38,14 +47,24 @@ public void keyPressed(KeyEvent e) {
 	// TODO Auto-generated method stub
 	char input = e.getKeyChar();
 	//System.out.println("Normie, you typed " + input);
+	
+	
+	if(charactersTyped == 100) {
+		showTypingSpeed(correctChar);
+	}
 	if(input == currentLetter) {
-		
-		
+		correctChar++;
+		charactersTyped++;
 		frame.getContentPane().setBackground(Color.GREEN);
 	}
     if(input != currentLetter) {
     	frame.getContentPane().setBackground(Color.RED);
+    	charactersTyped++;
+    
+
     }
+
+
 }
 @Override
 public void keyReleased(KeyEvent e) {
@@ -59,4 +78,20 @@ public void keyTyped(KeyEvent e) {
 	// TODO Auto-generated method stub
 	
 }
+Date timeAtStart = new Date();
+
+private void showTypingSpeed(int numberOfCorrectCharactersTyped) {
+      Date timeAtEnd = new Date();
+      long gameDuration = timeAtEnd.getTime() - timeAtStart.getTime();
+      long gameInSeconds = (gameDuration / 1000) % 60;
+      double charactersPerSecond = ((double) numberOfCorrectCharactersTyped / (double) gameInSeconds);
+      int charactersPerMinute = (int) (charactersPerSecond * 60);
+      JOptionPane.showMessageDialog(null, "Your typing speed is " + charactersPerMinute + " characters per minute.");
 }
+
+}
+
+
+
+
+
