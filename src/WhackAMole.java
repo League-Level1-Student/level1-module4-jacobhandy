14ae62876728ee641c9d0dd8bf33406971466023
student <student@SEMS-259-31.SEMS259.LAB>
2019-04-10 16:27:29 -0700

    whack a mole yeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeet

--- src/WhackAMole.java
+++ src/WhackAMole.java
@@ -5,34 +5,68 @@ import java.util.Random;
 
 import javax.swing.JButton;
 import javax.swing.JFrame;
+import javax.swing.JLabel;
 import javax.swing.JPanel;
+
+import com.sun.swing.internal.plaf.basic.resources.basic;
 
 public class WhackAMole implements MouseListener {
 JFrame frame;
 JPanel panel;
 Dimension d = new Dimension(100,25);
-	void drawButtons(Random r) {
-		for(int a = 0; a<24; a++) {
-			JButton b = new JButton();
+	JButton b;
+	int score = 0;
+	
+	JLabel points;
+public void drawButtons() {
+	Random r = new Random();
+	int z = r.nextInt(24);
+	for(int a = 0; a<24; a++) {
+			b = new JButton();
 			b.setPreferredSize(d);
 			b.addMouseListener(this);
+			b.setSize(d);
+			if(z == a) {
+				b.setText("Jacob");
+			}
 			panel.add(b);
 		}
 	}
 	void createUI() {
+
 	frame = new JFrame();
 	panel = new JPanel();
 	frame.setVisible(true);
-	
+	points = new JLabel();
+	 
 	frame.add(panel);
 	frame.setSize(275, 500);
+	
+	
+	
+	
+
 }
 
 	@Override
-	public void mouseClicked(MouseEvent arg0) {
+	public void mouseClicked(MouseEvent e) {
 		// TODO Auto-generated method stub
-		
-	}
+		JButton b = (JButton)e.getSource();
+		 
+			if(b.getText().equals("Jacob")) {
+			  System.out.println("Yeet");
+			  panel.removeAll();
+			  drawButtons();
+			  panel.revalidate();
+			   
+			  
+		 }
+		 else {
+			 System.out.println("Normie Alert");
+			 drawButtons();
+		 }
+		}
+	 
 
 	@Override
 	public void mouseEntered(MouseEvent arg0) {
--- src/WhackAMoleRunner.java
+++ src/WhackAMoleRunner.java
@@ -1,8 +1,10 @@
-
+import java.util.Random;
 
 public class WhackAMoleRunner {
 public static void main(String[] args) {
 	WhackAMole x = new WhackAMole();
+
 	x.createUI();
+	x.drawButtons();
 }
 }
